c38c888d90963caca7e4db7ff36e3fdd7e96a2eb
Megha Bodke <megha.n.bodke@gmail.com>
2022-11-09 15:37:23 +0530

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # diagram-to-database
+
+In this project we have created database from diagram, and wrote required queries for it.
+
+## Getting Started
+
+This repository includes file with plain SQL that can be used to create a database:
+
+- Use [queries](./schema_based_on_diagram.sql) to create all tables.
+
+## ER DIAGRAM
+
+![ERD](./screenshot/ERD.png)
+
+## Authors
+
+👤 **Megha Bodke**
+
+- GitHub: [@megha-n-bodke](https://github.com/megha-n-bodke)
+- LinkedIn: [Megha Bodke](https://www.linkedin.com/in/megha-bodke/)
+
+**Bantelom Geto**
+
+- GitHub: [@megha-n-bodke](https://github.com/megha-n-bodke)
+- LinkedIn: [Megha Bodke](https://www.linkedin.com/in/megha-bodke/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Thanks to [Microverse](https://www.microverse.org/) for giving this opportunity to learn ...
+- Code Reviewers & Coding Partners.
+- Hat tip to anyone whose code was used.
+- Inspiration.
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -11,12 +11,7 @@ create table medical_histories(
    status varchar(255) not null
 );
 
-/* create table medical_history_details(
-    id serial not null primary key,
-    medical_history_id int not null references medical_histories(id),
-    description varchar(255) not null,
-    created_at timestamp not null
-); */
+
 
 create table treatments(
     id serial not null primary key,
@@ -39,6 +34,14 @@ create table invoice_items(
     invoice_id int not null references invoices(id),
     treatment_id int not null references treatments(id)
 );
+/* many to many table */
+ create table medical_history_details(
+    id serial not null primary key,
+    medical_history_id int not null references medical_histories(id),
+    treatmnets_id int not null references treatments(id),
+    description varchar(255) not null,
+   
+); 
 
 
 /* Create Index */
@@ -46,3 +49,5 @@ CREATE INDEX medical_histories_patient_id_idx ON medical_histories(patient_id);
 CREATE INDEX invoices_medical_history_id_idx ON invoices(medical_history_id);   
 CREATE INDEX invoice_items_invoice_id_idx ON invoice_items(invoice_id); 
 CREATE INDEX invoice_items_treatment_id_idx ON invoice_items(treatment_id);
+
+CREATE INDEX medical_history_details_medical_history_id_idx ON medical_history_details(medical_history_id);
